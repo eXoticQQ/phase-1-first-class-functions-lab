4159450ee1c41706d50afea553113ed13f4f397b
Vitalii <vetal.shtager@gmail.com>
2022-04-07 07:26:59 -0400

    commit tree c54b8863082f7e755d1bd527fb94d829ebc3798f

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+// function returnFirstTwoDrivers(driversArray){
+//     return driversArray.slice(0,2);
+// }
+// function returnLastTwoDrivers(driversArray){
+//     return driversArray.slice(2,4)
+// }
+// function returnFirstTwoDrivers(driversArray, ) {
+//     return returnFirstTwoDrivers(driversArray);
+//   }
+
+function returnFirstTwoDrivers(driversArray) {
+    return (driversArray.slice(0, 2))
+}
+
+function returnLastTwoDrivers(driversArray) {
+    return (driversArray.slice(2, 4))
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num1) {
+    return function(num2) {
+        return num1 * num2
+    }
+}
+
+function fareDoubler(fare) {
+    return fare * 2
+}
+
+function fareTripler(fare) {
+    return fare * 3
+}
+
+function selectDifferentDrivers( arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrayOfDrivers)
+} 
+
